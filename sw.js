24014f62f2a08da7529b96372606806852dd05eb
誠兆老師 <local@classroom-rpg.invalid>
2026-08-26 22:22:04 +0800

    deploy mobile registration recovery

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "classroom-rpg-v126-20260825-class-safety-v7";
+const CACHE = "classroom-rpg-v126-20260826-registration-v8";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./assets/app-icon.svg", "./vendor/firebase/firebase-app-compat.js", "./vendor/firebase/firebase-auth-compat.js", "./vendor/firebase/firebase-firestore-compat.js", "./vendor/qrcode/qrcode.min.js", "./Lession/Lessionindex.html", "./Lession/question-bank.html", "./Lession/question-bank-data.js", "./Lession/math-dungeon.html", "./Lession/學生名冊匯入範例.xlsx"];
 const NEVER_CACHE = /(?:firestore|googleapis|gstatic\.com\/firebasejs|identitytoolkit|securetoken)/i;
 
