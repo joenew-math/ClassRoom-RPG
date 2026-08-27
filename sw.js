0f0addbc873bf0a4561abd2a615dbdc8a4a671be
誠兆老師 <local@classroom-rpg.invalid>
2026-08-27 22:00:57 +0800

    feat: smooth 15v15 Dota and celebrate class unlocks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "classroom-rpg-v126-20260827-student-entry-split-v20";
+const CACHE = "classroom-rpg-v126-20260827-dota-15v15-input-v23";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./assets/app-icon.svg", "./vendor/firebase/firebase-app-compat.js", "./vendor/firebase/firebase-auth-compat.js", "./vendor/firebase/firebase-firestore-compat.js", "./vendor/qrcode/qrcode.min.js", "./Lession/Lessionindex.html", "./Lession/question-bank.html", "./Lession/question-bank-data.js", "./Lession/math-dungeon.html", "./Lession/學生名冊匯入範例.xlsx"];
 const NEVER_CACHE = /(?:firestore|googleapis|gstatic\.com\/firebasejs|identitytoolkit|securetoken)/i;
 
