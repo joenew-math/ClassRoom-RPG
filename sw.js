d220482feaf870b0df0ec737a9a6d8e0c30cf6b7
誠兆老師 <local@classroom-rpg.invalid>
2026-08-28 00:24:05 +0800

    Lock quiz gestures and submitted answers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "classroom-rpg-v126-20260828-touch-zone-v30";
+const CACHE = "classroom-rpg-v126-20260828-gesture-lock-v31";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./assets/app-icon.svg", "./vendor/firebase/firebase-app-compat.js", "./vendor/firebase/firebase-auth-compat.js", "./vendor/firebase/firebase-firestore-compat.js", "./vendor/qrcode/qrcode.min.js", "./Lession/Lessionindex.html", "./Lession/question-bank.html", "./Lession/question-bank-data.js", "./Lession/math-dungeon.html", "./Lession/學生名冊匯入範例.xlsx"];
 const NEVER_CACHE = /(?:firestore|googleapis|gstatic\.com\/firebasejs|identitytoolkit|securetoken)/i;
 
